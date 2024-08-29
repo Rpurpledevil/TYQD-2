72481285053c61396ad91723e1febb33be497944
Rpurpledevil <64737840+Rpurpledevil@users.noreply.github.com>
2024-08-29 14:48:17 +0800

    Update wxPusher.js

--- src/push/wxPusher.js
+++ src/push/wxPusher.js
@@ -1,5 +1,5 @@
 module.exports = {
   appToken:
-    process.env.WX_PUSHER_APP_TOKEN || "AT_OzwCik0QP4p4AQlKdO4jbgjWApTiWsJr",
+    process.env.WX_PUSHER_APP_TOKEN || "AT_FM1djM9wZ5mGdifCu0JDbXX7afEbb0xH",
   uid: process.env.WX_PUSHER_UID || "UID_pvAfpsriissg6fIqRLl1K6SLCGTs",
 };

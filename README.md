9559c33d4b25db34536d327ff10f038cbb762cbc
Rpurpledevil <64737840+Rpurpledevil@users.noreply.github.com>
2024-08-29 09:10:20 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,123 @@
-# -2
+# Cloud189Checkin
+
+天翼网盘自动签到（随机容量) 和抽奖（两次，每次 50M，共 150M）获取空间，家庭空间签到（随机容量）。
+
+# 重要说明！！！
+
+请勿直接修改 .env，然后提交到 github，源码仓库是公开的，别人可以直接看到你的账号密码。因为错误使用本仓库导致账号密码泄漏，本人概不负责！！！
+
+## **目录**
+
+- [GitHub Action 运行](#GitHubAction运行)
+- [本地运行](#本地运行)
+- [其他环境集成](#其他环境集成)
+- [交流群](#交流群)
+- [更新内容](#更新内容)
+
+## GitHub Action 运行
+
+### Fork 此仓库
+
+![](https://cdn.jsdelivr.net/gh/wes-lin/Cloud189Checkin/image/fork.png)
+
+### 设置账号密码
+
+新版本的 git Action 需要创建 environment 来配合使用，创建一个名为 user 的环境。
+![](https://cdn.jsdelivr.net/gh/wes-lin/Cloud189Checkin/image/env.png)
+创建好后编辑 user 环境，添加变量 TY_ACCOUNTS, userName 和 password 为你的天翼账号和密码,可以添加多个账号如[{"userName":"账号 1","password":"账号 1 的密码"},{"userName":"账号 2","password":"账号 2 的密码"}]
+![](https://cdn.jsdelivr.net/gh/wes-lin/Cloud189Checkin/image/accounts.jpg)
+
+如果你遇到你账号密码中有特殊字符如#$等无法解析的[SyntaxError](https://github.com/wes-lin/Cloud189Checkin/issues/76),请在你的配置中将TY_ACCOUNTS用单引号包起来
+例如'[{"userName":"1234567890","password":"123334#$#$"}]'
+
+
+### 设置推送
+
+#### Server 酱
+
+为了考虑到不同客户端兼容性,采用了 Server 酱,只需多配置下 SENDKEY
+![](https://cdn.jsdelivr.net/gh/wes-lin/Cloud189Checkin/image/push.png)就行,Server 酱的配置和 sendkey 的获取可参看[Server 酱官网](https://sct.ftqq.com/)
+
+#### TelegramBot 推送
+
+- `TELEGRAM_BOT_TOKEN` _Telegram Bot Token_
+- `TELEGRAM_CHAT_ID` _Telegram 接收推送消息的会话 ID_
+
+#### 微信群机器人推送
+
+- `WECOM_BOT_KEY ` _微信群机器人 webhook_
+- `WECOM_BOT_TELPHONE ` _接收推送手机号_
+  [群机器人配置说明](https://developer.work.weixin.qq.com/document/path/91770)
+
+#### WxPusher 推送
+
+- `WX_PUSHER_APP_TOKEN ` _WxPuser 推送 AppToken_
+- `WX_PUSHER_UID ` _接收推送 UID_
+  默认使用是我的 WxPusher,你也可以改成你自己 wxPusher 开发者账户,修改 WX_PUSHER_APP_TOKEN. 如果想直接使用我的 wxPush 应用,请扫描底下二维码进行关联.
+  https://wxpusher.zjiecode.com/api/qrcode/4Ix7noqD3L7DMBoSlvig3t4hqjFWzPkdHqAYsg8IzkPreW7d8uGUHi9LJO4EcyJg.jpg
+  然后拿到 UID 后,把 WX_PUSHER_UID 配成你拿到的 UID.
+  ![](https://cdn.jsdelivr.net/gh/wes-lin/Cloud189Checkin/image/wxpusher.jpg)
+
+### 执行任务
+
+1. 点击**Action**，再点击**I understand my workflows, go ahead and enable them**
+2. 给自己仓库点个 start 或者修改任意文件后提交一次或者手动点击运行
+   ![](http://tu.yaohuo.me/imgs/2020/06/34ca160c972b9927.png)
+3. 每天早上 10 点执行任务
+
+### 查看运行结果
+
+Actions > Cloud check in action > build
+![](https://cdn.jsdelivr.net/gh/wes-lin/Cloud189Checkin/image/action.png)
+
+## 本地运行
+
+### 环境配置
+
+```
+Node.js 18+
+```
+
+### 克隆项目
+
+```bash
+git clone https://github.com/wes-lin/Cloud189Checkin.git
+```
+
+```bash
+cd Cloud189Checkin
+```
+
+### 安装依赖
+
+```bash
+npm install
+```
+
+### 运行
+
+​ 修改源码中.env 中 userName 和 password 为你的天翼账号和密码,可以添加多个账号如[{"userName":"账号 1","password":"账号 1 的密码"},{"userName":"账号 2","password":"账号 2 的密码"}]
+
+```bash
+TY_ACCOUNTS=[{"userName":"userName","password":"password"}]
+```
+
+### 推送
+
+修改 serverChan.js 或者添加环境变量 SENDKEY
+
+执行命令
+
+```bash
+npm start
+```
+
+## 其他环境集成
+
+我已经把天翼网盘的相关 API 集成到[SDK](https://github.com/wes-lin/cloud189-sdk)了，有编程能力的同学可以自行拓展，集成到自己的代码环境。
+
+## 交流群
+
+![](https://cdn.jsdelivr.net/gh/wes-lin/Cloud189Checkin/image/group.jpg)
+
+## [更新内容](https://github.com/wes-lin/Cloud189Checkin/wiki/更新内容)
---
+++ src/app.js
@@ -0,0 +1,246 @@
+/* eslint-disable no-await-in-loop */
+require("dotenv").config();
+const log4js = require("log4js");
+const recording = require("log4js/lib/appenders/recording");
+log4js.configure({
+  appenders: {
+    vcr: {
+      type: "recording",
+    },
+    out: {
+      type: "console",
+    },
+  },
+  categories: { default: { appenders: ["vcr", "out"], level: "info" } },
+});
+
+const logger = log4js.getLogger();
+// process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0'
+const superagent = require("superagent");
+const { CloudClient } = require("cloud189-sdk");
+const serverChan = require("./push/serverChan");
+const telegramBot = require("./push/telegramBot");
+const wecomBot = require("./push/wecomBot");
+const wxpush = require("./push/wxPusher");
+const accounts = require("../accounts");
+
+const mask = (s, start, end) => s.split("").fill("*", start, end).join("");
+
+const buildTaskResult = (res, result) => {
+  const index = result.length;
+  if (res.errorCode === "User_Not_Chance") {
+    result.push(`第${index}次抽奖失败,次数不足`);
+  } else {
+    result.push(`第${index}次抽奖成功,抽奖获得${res.prizeName}`);
+  }
+};
+
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+// 任务 1.签到 2.天天抽红包 3.自动备份抽红包
+const doTask = async (cloudClient) => {
+  const result = [];
+  const res1 = await cloudClient.userSign();
+  result.push(
+    `${res1.isSign ? "已经签到过了，" : ""}签到获得${res1.netdiskBonus}M空间`
+  );
+  await delay(5000); // 延迟5秒
+
+  const res2 = await cloudClient.taskSign();
+  buildTaskResult(res2, result);
+
+  await delay(5000); // 延迟5秒
+  const res3 = await cloudClient.taskPhoto();
+  buildTaskResult(res3, result);
+
+  return result;
+};
+
+const doFamilyTask = async (cloudClient) => {
+  const { familyInfoResp } = await cloudClient.getFamilyList();
+  const result = [];
+  if (familyInfoResp) {
+    for (let index = 0; index < familyInfoResp.length; index += 1) {
+      const { familyId } = familyInfoResp[index];
+      const res = await cloudClient.familyUserSign(familyId);
+      result.push(
+        "家庭任务" +
+          `${res.signStatus ? "已经签到过了，" : ""}签到获得${
+            res.bonusSpace
+          }M空间`
+      );
+    }
+  }
+  return result;
+};
+
+const pushServerChan = (title, desp) => {
+  if (!serverChan.sendKey) {
+    return;
+  }
+  const data = {
+    title,
+    desp,
+  };
+  superagent
+    .post(`https://sctapi.ftqq.com/${serverChan.sendKey}.send`)
+    .type("form")
+    .send(data)
+    .end((err, res) => {
+      if (err) {
+        logger.error(`ServerChan推送失败:${JSON.stringify(err)}`);
+        return;
+      }
+      const json = JSON.parse(res.text);
+      if (json.code !== 0) {
+        logger.error(`ServerChan推送失败:${JSON.stringify(json)}`);
+      } else {
+        logger.info("ServerChan推送成功");
+      }
+    });
+};
+
+const pushTelegramBot = (title, desp) => {
+  if (!(telegramBot.botToken && telegramBot.chatId)) {
+    return;
+  }
+  const data = {
+    chat_id: telegramBot.chatId,
+    text: `${title}\n\n${desp}`,
+  };
+  superagent
+    .post(`https://api.telegram.org/bot${telegramBot.botToken}/sendMessage`)
+    .type("form")
+    .send(data)
+    .end((err, res) => {
+      if (err) {
+        logger.error(`TelegramBot推送失败:${JSON.stringify(err)}`);
+        return;
+      }
+      const json = JSON.parse(res.text);
+      if (!json.ok) {
+        logger.error(`TelegramBot推送失败:${JSON.stringify(json)}`);
+      } else {
+        logger.info("TelegramBot推送成功");
+      }
+    });
+};
+
+const pushWecomBot = (title, desp) => {
+  if (!(wecomBot.key && wecomBot.telphone)) {
+    return;
+  }
+  const data = {
+    msgtype: "text",
+    text: {
+      content: `${title}\n\n${desp}`,
+      mentioned_mobile_list: [wecomBot.telphone],
+    },
+  };
+  superagent
+    .post(
+      `https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=${wecomBot.key}`
+    )
+    .send(data)
+    .end((err, res) => {
+      if (err) {
+        logger.error(`wecomBot推送失败:${JSON.stringify(err)}`);
+        return;
+      }
+      const json = JSON.parse(res.text);
+      if (json.errcode) {
+        logger.error(`wecomBot推送失败:${JSON.stringify(json)}`);
+      } else {
+        logger.info("wecomBot推送成功");
+      }
+    });
+};
+
+const pushWxPusher = (title, desp) => {
+  if (!(wxpush.appToken && wxpush.uid)) {
+    return;
+  }
+  const data = {
+    appToken: wxpush.appToken,
+    contentType: 1,
+    summary: title,
+    content: desp,
+    uids: [wxpush.uid],
+  };
+  superagent
+    .post("https://wxpusher.zjiecode.com/api/send/message")
+    .send(data)
+    .end((err, res) => {
+      if (err) {
+        logger.error(`wxPusher推送失败:${JSON.stringify(err)}`);
+        return;
+      }
+      const json = JSON.parse(res.text);
+      if (json.data[0].code !== 1000) {
+        logger.error(`wxPusher推送失败:${JSON.stringify(json)}`);
+      } else {
+        logger.info("wxPusher推送成功");
+      }
+    });
+};
+
+const push = (title, desp) => {
+  pushServerChan(title, desp);
+  pushTelegramBot(title, desp);
+  pushWecomBot(title, desp);
+  pushWxPusher(title, desp);
+};
+
+// 开始执行程序
+async function main() {
+  for (let index = 0; index < accounts.length; index += 1) {
+    const account = accounts[index];
+    const { userName, password } = account;
+    if (userName && password) {
+      const userNameInfo = mask(userName, 3, 7);
+      try {
+        logger.log(`账户 ${userNameInfo}开始执行`);
+        const cloudClient = new CloudClient(userName, password);
+        await cloudClient.login();
+        const result = await doTask(cloudClient);
+        result.forEach((r) => logger.log(r));
+        const familyResult = await doFamilyTask(cloudClient);
+        familyResult.forEach((r) => logger.log(r));
+        logger.log("任务执行完毕");
+        const { cloudCapacityInfo, familyCapacityInfo } =
+          await cloudClient.getUserSizeInfo();
+        logger.log(
+          `个人总容量：${(
+            cloudCapacityInfo.totalSize /
+            1024 /
+            1024 /
+            1024
+          ).toFixed(2)}G,家庭总容量：${(
+            familyCapacityInfo.totalSize /
+            1024 /
+            1024 /
+            1024
+          ).toFixed(2)}G`
+        );
+      } catch (e) {
+        logger.error(e);
+        if (e.code === "ETIMEDOUT") {
+          throw e;
+        }
+      } finally {
+        logger.log(`账户 ${userNameInfo}执行完毕`);
+      }
+    }
+  }
+}
+
+(async () => {
+  try {
+    await main();
+  } finally {
+    const events = recording.replay();
+    const content = events.map((e) => `${e.data.join("")}`).join("  \n");
+    push("天翼云盘自动签到任务", content);
+    recording.erase();
+  }
+})();
